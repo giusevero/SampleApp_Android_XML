9da36fe16e34efb45beff0bbed021f3ccff8ec27
gsantose <gsantose@everis.com>
2020-12-17 13:35:42 -0300

    🎉 | 🚧 First commit for sample app

---
+++ app/src/main/java/com/example/sampleapp_android/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sampleapp_android.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sampleapp_android.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/sampleapp_android/service/Api.kt
@@ -0,0 +1,7 @@
+package com.example.sampleapp_android.service
+
+import com.example.sampleapp_android.BuildConfig
+
+class Api {
+    private val BASE_URL = BuildConfig.SERVER_URL
+}
